79f2245676cbfe292e20483a0f13e293134d97e6
agridnev228 <agridnev228@gmail.com>
2021-10-25 21:39:07 +0500

    task03

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,7 +3,6 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
         /*
         throwCheckedException();
          */
