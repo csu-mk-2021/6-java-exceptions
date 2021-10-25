f4b8c0eb6f4d4ffaf28470767c934574c8a58b33
agridnev228 <agridnev228@gmail.com>
2021-10-25 21:41:43 +0500

    task03

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -3,7 +3,6 @@ package com.example.task06;
 public class Task06Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
         /*
         new Task06Main().printMethodName();
          */
