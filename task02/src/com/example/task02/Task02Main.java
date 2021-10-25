0695c3302f8de725eb8576fdf1614386f6b0b43f
agridnev228 <agridnev228@gmail.com>
2021-10-25 21:37:47 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,9 +5,9 @@ public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getSeason(-5));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
