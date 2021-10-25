45a2779dd93c911ea20df652784afa261cbc781d
EstSov <fedorovama@gmail.com>
2021-10-25 16:38:31 +0500

    task02 push again

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -11,7 +11,7 @@ public class Task02Main {
     }
 
     static String getSeason(int monthNumber) {
-        if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) return "зима";
+        if (monthNumber == 12 || monthNumber == 1 || monthNumber == 2) return "зима";//push again
         else if (monthNumber >= 3 && monthNumber <= 5) return "весна";
         else if (monthNumber >= 6 && monthNumber <= 8) return "лето";
         else if (monthNumber >= 9 && monthNumber <= 11) return "осень";
