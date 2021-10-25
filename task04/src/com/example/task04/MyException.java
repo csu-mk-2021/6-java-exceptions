0bdc4c261470f2fe85f078f453eb452e2656c605
EstSov <fedorovama@gmail.com>
2021-10-25 16:40:00 +0500

    task04 push again

--- task04/src/com/example/task04/MyException.java
+++ task04/src/com/example/task04/MyException.java
@@ -1,6 +1,6 @@
 package com.example.task04;
 
-public class MyException extends IllegalArgumentException{
+public class MyException extends IllegalArgumentException{//push again
     public MyException(String message) {
         super(message);
     }
