085e1f1def3b5766a72144bfc144d4d9e983d232
agridnev228 <agridnev228@gmail.com>
2021-10-25 21:36:20 +0500

    task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -4,9 +4,9 @@ public class Task01Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         codeWithNPE();
-         */
+
     }
 
     static void codeWithNPE() {
