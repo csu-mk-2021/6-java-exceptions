62aa299f33bd64b5bf1f252f474727743d06be8d
raion2578 <ivan.baldin@bk.ru>
2021-10-28 02:09:42 +0500

    hot fix

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,11 +3,9 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
-        throwCheckedException();
-
     }
 
     public static void throwCheckedException() throws Exception {
-        throw new Exception("Exception comment");
+        throw new Exception("Exception 404");
     }
 }
